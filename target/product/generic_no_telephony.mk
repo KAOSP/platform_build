a19091b11496be56742bbac3fbb8fd952494bf4c
Pedro Kalil <kalilgamer0@gmail.com>
2018-05-12 05:02:32 +0000

    generic: Enable navbar

--- target/product/generic_no_telephony.mk
+++ target/product/generic_no_telephony.mk
@@ -79,7 +79,8 @@ DMTIMESTAMP := $(shell date +%d-%m)
 KAOSP_VERSION := KAOSP_$(TARGET_PRODUCT)_$(DMTIMESTAMP)
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.kaosp.version=$(KAOSP_VERSION) \
-    ro.modversion=$(KAOSP_VERSION)
+    ro.modversion=$(KAOSP_VERSION)\
+    qemu.hw.mainkeys=0
 
 # Themes
 PRODUCT_PROPERTY_OVERRIDES += ro.boot.vendor.overlay.theme=com.google.android.theme.pixel
